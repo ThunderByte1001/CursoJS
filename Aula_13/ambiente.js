bf5cc34a2c41098ba993134d4702cd4cccc422bd
Daniel <130584997+ThunderByte1001@users.noreply.github.com>
2023-06-08 17:49:20 +0100

    Add files via upload

---
+++ Aula_13/ambiente.js
@@ -0,0 +1,25 @@
+  /*  var contador = 1
+while (contador <= 6) {
+    console.log(`Passo ${contador}`)
+    contador = contador +1 //ou contador++
+}
+*/
+
+/*console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?
+console.log('Tudo bem?
+console.log('Tudo bem?
+console.log('Tudo bem?
+console.log('Tudo bem?
+*/
+
+var contador = 1
+do {
+    console.log(`Passo ${contador}`)
+    contador= contador +1
+}while(contador <= 6)
+
+
+
+

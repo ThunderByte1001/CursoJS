46632e0230e1fe79b2cc51a4cfd016763874b8b5
Daniel <130584997+ThunderByte1001@users.noreply.github.com>
2023-06-21 15:35:11 +0100

    praticando

---
+++ Aula_14/ambiente_teste.js
@@ -0,0 +1,19 @@
+/*
+var c = 1
+do {
+    console.log(c)
+    c++
+} while(c<=5)*/
+//----------//--------
+
+/*console.log('Vai comecar...')
+for (var c = 1 ; c <= 6 ; c++ ){
+    console.log(`Passo ${c}`)
+}
+console.log('FIM!')*/
+var numero = 10
+var c = 1
+while (c <= 10){
+    console.log(`${numero} x ${c} = ${numero * c}`)
+    c++
+}
